21a805600792c8a41adc7c47dafeb8324d9dcbed
Martin Reyes <martinreyesrf@gmail.com>
2022-09-19 18:04:37 -0500

    estilos crudos agregados

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useFetch } from './hooks/useFetch';
 import './App.css'
 import './styles.css'
 import { useState } from 'react';
+import { GitHubIco } from './assets/icos/GitHub';
+import { LinkedInIco } from './assets/icos/LinkedIn';
 
 function App() {
   const [puntaje, setPuntaje] = useState(0)
@@ -16,22 +18,22 @@ function App() {
         <header className='header'>
          <div className='header__buttons'>
            <p className='header__buttons_item'>Reiniciar</p>
-           <p className='header__buttons_item'>Sin funcion</p>
+           {/* <p className='header__buttons_item'>Sin funcion</p> */}
          </div>
-         <h1>Memorama Rick and Morty</h1>
-         <h2> Puntaje: {puntaje}</h2>
+         <h1 className='header__titulo'>Memorama Rick and Morty</h1>
+         <h2 className='header__movimientos'> Movimientos: <span>{puntaje}</span></h2>
         </header>
         <Tablero baraja={baraja} setBarajaPrime={setBarajaPrime} puntaje={(value) => setPuntaje(value)} />
       </div>
  
       <footer className='flex-box-column'>
-        <p className='footer__by'>Elaborado por: Martin Reyes</p>
+        <p className='footer__by'>Elaborado by: Martin Reyes</p>
         {/* Redes */}
         <div className='center-flex-box'>
-          <p>Icono de GitHub</p>
-          <p> Icono de Linkden</p>
+          <a href='https://github.com/MartiRF' target="_blank" ><GitHubIco />  GitHub</a>
+          <a href='https://www.linkedin.com/in/martin-reyes-95743a248/' target="_blank" ><LinkedInIco />  LinkedIn</a>
         </div>
-        <p className='footer__by'><a>Ir al repositorio</a></p>
+        <a className='center-flex-box' href='https://github.com/MartiRF/React-RickAndMorty-Memorama'>Ir al repositorio</a>
       </footer>
 
     </>
